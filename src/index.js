be4e5f7ffb67bd6bc84ee2f7dd29bf2c6abcd037
Maria Shklovskaya <mariashklovskaya@coherentsolutions.com>
2018-09-13 14:10:18 +0300

    first attempt

--- src/index.js
+++ src/index.js
@@ -1,26 +1,77 @@
+// FIRST IMPLEMENTATION (part test passing)
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.numValue = initialValue;
+  }
+
+  toString() {
+    return this.numValue;
+  }
+
+  valueOf() {
+    return this.numValue;
   }
 
   add(number) {
-    // your implementation
+    this.numValue+=number;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.numValue-=number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.numValue*=number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.numValue/=number;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.numValue = this.numValue**number;
+    return this;
+  }
+}
+
+module.exports = SmartCalculator;
+
+//  SECOND IMPLEMENTATION (test passing)
+class SmartCalculator {
+  constructor(initialValue) {
+    this.tempValue = initialValue.toString();
+  }
+  
+  add(number) {
+    this.tempValue += '+' + number;
+    return this;
+  }
+  
+  subtract(number) {
+    this.tempValue += '-' + number;
+    return this;
+  }
+
+  multiply(number) {
+    this.tempValue += '*' + number;
+    return this;
+  }
+
+  devide(number) {
+    this.tempValue += '/' + number;
+    return this;
+  }
+
+  pow(number) {
+    this.tempValue+= '**' + number;
+    return this;
+  }
+  valueOf(){
+    return eval(this.tempValue);
   }
 }
 
